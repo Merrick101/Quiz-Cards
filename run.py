e77ef234de3d103e70d3b8bd1f8a5a44a3e75be3
Merrick Minogue <yt7i0aoi@students.codeinstitute.net>
2024-10-25 15:17:18 +0000

    Initial commit, added function for main menu options

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main_menu():
+    while True:
+        print("\nFlashcards Learning Tool")
+        print("1. Add a Flashcard")
+        print("2. View Flashcards")
+        print("3. Start Quiz")
+        print("4. Delete a Flashcard")
+        print("5. Exit")
+
+        choice = input("Please select an option (1-6): ")
+
+        if choice == "1":
+            add_flashcard()
+        elif choice == "2":
+            view_flashcards()
+        elif choice == "3":
+            start_quiz()
+        elif choice == "4":
+            delete_flashcard()
+        elif choice == "5":
+            save_flashcards()
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option. Please try again.")
+
